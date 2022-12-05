daa229db35334139f4ace81ac9d50cc5d17cf296
Mike K <Shmellrzn@gmail.com>
2022-12-05 14:45:02 +0300

    edited fourth task

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//Домашнее задание №1
 //Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите 1-е число: ");
+/*Console.Write("Введите 1-е число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-е число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +14,18 @@ int num2 = Convert.ToInt32(Console.ReadLine());
     {
     Console.Write(num2 + " больше " + num1 + " потрясающе!");
     }
+*/
 
 
+
+//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+/*Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int cur = 1;
+while(cur <= num)
+{
+    Console.Write(cur + " ");
+    cur++;
+}*/
 
